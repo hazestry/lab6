5a7f16a9aba37e1d50639765dbe6610d1f3c0ab3
hazestry <hzstrl@mail.ru>
2025-11-21 10:50:50 +0500

    добавлены наборы представлений, маршруты аутентификации JWT и пользовательский интерфейс Swagger с пагинацией

--- api/views.py
+++ api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.pagination import PageNumberPagination
+from .models import Client, Car, Order
+from .serializers import ClientSerializer, CarSerializer, OrderSerializer
 
-# Create your views here.
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
